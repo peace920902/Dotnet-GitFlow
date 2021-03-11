11e7512edbe0f1171ccfecd421e3c0d7c7d5125e
peace920902 <peace9209021@gmail.com>
2021-03-11 11:41:38 +0800

    add temperature transfer and test

---
+++ Dotnetcore-Gitflow-Test/TemperatureTransfer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Dotnetcore_Gitflow_Test
+{
+    public class TemperatureTransfer
+    {
+        public double ConvertToF(double c)
+        {
+            return Math.Round(c * 9 / 5 + 32, 2);
+        }
+
+        public double ConvertToC(double f)
+        {
+            return Math.Round((f - 32) * 5 / 9, 2);
+        }
+    }
+}
---
+++ Dotnetcore-Gitflow-TestTests/TemperatureTransferTests.cs
@@ -0,0 +1,42 @@
+﻿using Xunit;
+using Dotnetcore_Gitflow_Test;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dotnetcore_Gitflow_Test.Tests
+{
+    public class TemperatureTransferTests
+    {
+        private readonly TemperatureTransfer _temperatureTransfer;
+
+        public TemperatureTransferTests()
+        {
+            _temperatureTransfer = new();
+        }
+        
+        [Theory]
+        [InlineData(0,32)]
+        [InlineData(25, 77)]
+        [InlineData(-273.15, -459.67)]
+        [InlineData(10.54, 50.97)]
+        public void ConvertToFTest(double input,double result )
+        {
+            var actual = _temperatureTransfer.ConvertToF(input);
+            Assert.Equal(actual, result);
+        }
+
+        [Theory]
+        [InlineData(32, 0)]
+        [InlineData(77, 25)]
+        [InlineData(-459.67, -273.15)]
+        [InlineData(50.97, 10.54)]
+        public void ConvertToCTest(double input, double result)
+        {
+            var actual = _temperatureTransfer.ConvertToC(input);
+            Assert.Equal(actual, result);
+        }
+    }
+}
