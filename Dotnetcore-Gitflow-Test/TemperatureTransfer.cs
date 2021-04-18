a73887e29320f90eae87d28a842c27426986a774
peace920902 <peace9209021@gmail.com>
2021-04-18 23:53:28 +0800

    remove redunt code

--- Dotnetcore-Gitflow-Test/TemperatureTransfer.cs
+++ Dotnetcore-Gitflow-Test/TemperatureTransfer.cs
@@ -6,87 +6,13 @@ namespace Dotnetcore_Gitflow_Test
 {
     public class TemperatureTransfer
     {
-        public async Task<double> ConvertToF(double c)
+        public double ConvertToF(double c)
         {
-            var x = 0;
-            if (x == 1)
-            {
-                Console.WriteLine(1);
-            }
-            else if (x == 2)
-            {
-                Console.WriteLine(1);
-            }
-            else if (x == 2)
-            {
-                Console.WriteLine(1);
-            }
-            else if (x == 2)
-            {
-                Console.WriteLine(1);
-            }
-            else if (x == 2)
-            {
-                Console.WriteLine(1);
-            }
-            else if (x == 2)
-            {
-                Console.WriteLine(1);
-            }
-            else if (x == 2)
-            {
-                Console.WriteLine(1);
-            }
-            else if (x == 2)
-            {
-                Console.WriteLine(1);
-            }
-            else
-            {
-
-            }
             return Math.Round(c * 9 / 5 + 32, 2);
         }
 
-        public async Task<double> ConvertToC(double f)
+        public double ConvertToC(double f)
         {
-            var x = 0;
-            if (x == 1)
-            {
-                Console.WriteLine(1);
-            }
-            else if (x == 2)
-            {
-                Console.WriteLine(1);
-            }
-            else if (x == 2)
-            {
-                Console.WriteLine(1);
-            }
-            else if (x == 2)
-            {
-                Console.WriteLine(1);
-            }
-            else if (x == 2)
-            {
-                Console.WriteLine(1);
-            }
-            else if (x == 2)
-            {
-                Console.WriteLine(1);
-            }
-            else if (x == 2)
-            {
-                Console.WriteLine(1);
-            }
-            else if (x == 2)
-            {
-                Console.WriteLine(1);
-            }
-            else
-            {
-
-            }
             return Math.Round((f - 32) * 5 / 9, 2);
         }
     }
